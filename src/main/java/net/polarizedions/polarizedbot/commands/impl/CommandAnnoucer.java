273e53346422d734d4779f931427a03f5ebd8523
PolarizedIons <PolarizedIons@gmail.com>
2018-08-28 17:00:37 +0200

    Add ability to see what channels are subscribed to what announcements

--- src/main/java/net/polarizedions/polarizedbot/commands/impl/CommandAnnoucer.java
+++ src/main/java/net/polarizedions/polarizedbot/commands/impl/CommandAnnoucer.java
@@ -12,51 +12,47 @@ import sx.blah.discord.handle.obj.IChannel;
 import sx.blah.discord.handle.obj.IMessage;
 
 import java.util.List;
+import java.util.Map;
 import java.util.function.BiConsumer;
 
 public class CommandAnnoucer implements ICommand {
-
+    private static String[] subcommands = new String[] {"subscribe", "unsubscribe", "list", "guild"};
     @Override
     public CommandTree getCommand() {
         return CommandBuilder.create("Announcer")
-                .setRank(UserRank.GUILD_ADMIN)
-                .setHelp("announces things")
-                .command("announce", announce -> announce
-                        .optionArg(new String[]{"subscribe", "sub"}, sub -> sub
-                            .captureArg(name -> name
-                                .channelArg(channel -> channel
-                                        .onExecute((msg, args) -> this.manageSubscription(msg, true, (String)args.get(2), (IChannel)args.get(3)))
-                                        .onFail(this::failChannel)
-                                )
-                                .onExecute((msg, args) -> this.manageSubscription(msg, true, (String)args.get(2), msg.getChannel()))
-                                .onFail(this::fail)
-                            )
-                        )
-                        .optionArg(new String[] {"unsubscribe", "unsub"}, unsub -> unsub
-                            .captureArg(name -> name
-                                .channelArg(channel -> channel
-                                    .onExecute((msg, args) -> this.manageSubscription(msg, false, (String)args.get(2), (IChannel)args.get(3)))
-                                    .onFail(this::failChannel)
-                                )
-                                .onExecute((msg, args) -> this.manageSubscription(msg, false, (String)args.get(2), msg.getChannel()))
-                                .onFail(this::fail)
-                            )
-                        )
-                        .stringArg("list", list -> list
-                            .onExecute(this::listAnnouncements)
-                        )
-                        .onFail(this::fail)
-                )
-                .buildCommand();
+                 .setRank(UserRank.GUILD_ADMIN)
+                 .setHelp("announces things")
+                 .command("announce", announce -> announce
+                         .optionArg(new String[]{"subscribe", "sub"}, sub -> sub
+                                 .captureArg(name -> name
+                                         .channelArg(channel -> channel
+                                                 .onExecute((msg, args) -> this.manageSubscription(msg, true, (String) args.get(2), (IChannel) args.get(3)))
+                                                 .onFail(this::failChannel)
+                                         )
+                                         .onExecute((msg, args) -> this.manageSubscription(msg, true, (String) args.get(2), msg.getChannel()))
+                                         .onFail(this::fail)
+                                 )
+                         )
+                         .optionArg(new String[]{"unsubscribe", "unsub"}, unsub -> unsub
+                                 .captureArg(name -> name
+                                         .channelArg(channel -> channel
+                                                 .onExecute((msg, args) -> this.manageSubscription(msg, false, (String) args.get(2), (IChannel) args.get(3)))
+                                                 .onFail(this::failChannel)
+                                         )
+                                         .onExecute((msg, args) -> this.manageSubscription(msg, false, (String) args.get(2), msg.getChannel()))
+                                         .onFail(this::fail)
+                                 )
+                         )
+                         .stringArg("list", list -> list
+                                 .onExecute(this::listAnnouncements)
+                         )
+                         .stringArg("guild", guild -> guild
+                                 .onExecute(this::listGuild)
+                         )
+                         .onFail(this::fail)
+                 )
+                 .buildCommand();
     }
-
-
-    private void listAnnouncements(IMessage message, List<Object> args) {
-        AnnouncerManager announcerManager = Bot.instance.getAnnouncerManager();
-        String announcers = String.join(", ", announcerManager.getNames());
-        message.getChannel().sendMessage(Localizer.localize("command.announce.list", announcers));
-    }
-
 
     private void manageSubscription(IMessage message, boolean isSub, String announcerName, IChannel channel) {
         AnnouncerManager announcerManager = Bot.instance.getAnnouncerManager();
@@ -72,19 +68,47 @@ public class CommandAnnoucer implements ICommand {
         message.getChannel().sendMessage(Localizer.localize("command.announce.success." + (isSub ? "sub" : "unsub"), announcer.getName(), channel.toString()));
     }
 
+    private void listAnnouncements(IMessage message, List<Object> args) {
+        AnnouncerManager announcerManager = Bot.instance.getAnnouncerManager();
+        String announcers = String.join(", ", announcerManager.getNames());
+        message.getChannel().sendMessage(Localizer.localize("command.announce.list", announcers));
+    }
+
     private void fail(IMessage message, List<Object> parsedArgs, List<String> unparsedArgs) {
         if (parsedArgs.size() == 1) {
-            message.getChannel().sendMessage(Localizer.localize("command.announce.error.no_subcommand", "subscribe, unsubscribe, list"));
-        }
-        else if (parsedArgs.size() == 2){
+            message.getChannel().sendMessage(Localizer.localize("command.announce.error.no_subcommand", String.join(", ", subcommands)));
+        } else if (parsedArgs.size() == 2) {
             message.getChannel().sendMessage(Localizer.localize("command.announce.error.channel_or_announcer_missing"));
-        }
-        else {
+        } else {
             message.getChannel().sendMessage(Localizer.localize("command.announce.error.announcer_missing"));
         }
     }
 
     private void failChannel(IMessage message, List<Object> parsedArgs, List<String> unparsedArgs) {
         message.getChannel().sendMessage(Localizer.localize("command.announce.error.no_channel"));
+    }
+
+    private void listGuild(IMessage message, List<Object> args) {
+        Map<IAnnouncer, List<IChannel>> announcers = Bot.instance.getAnnouncerManager().getAnnouncersForGuild(message.getGuild());
+
+        if (announcers.size() == 0) {
+            message.getChannel().sendMessage(Localizer.localize("command.announce.error.no_announcements"));
+            return;
+        }
+
+        StringBuilder response = new StringBuilder("```\n");
+        for (Map.Entry<IAnnouncer, List<IChannel>> entry : announcers.entrySet()) {
+            List<IChannel> channels = entry.getValue();
+            response.append(" * ").append(entry.getKey().getName()).append(": ").append("#").append(channels.get(0).getName()).append("\n");
+            int len = 3 + entry.getKey().getName().length() + 2;
+            for (int i = 1; i < channels.size(); i++) {
+                for (int j = 0; j < len; j++) {
+                    response.append(" ");
+                }
+                response.append("#").append(channels.get(i).getName()).append("\n");
+            }
+        }
+        response.append("```");
+        message.getChannel().sendMessage(response.toString());
     }
 }

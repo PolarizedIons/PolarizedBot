ae08c79040b3866fc9ee6b06afa6f2c5391f38a3
PolarizedIons <polarizedions@gmail.com>
2018-08-30 00:01:31 +0200

    Cleanup CommandManager a but & make commands run in async

--- src/main/java/net/polarizedions/polarizedbot/commands/CommandManager.java
+++ src/main/java/net/polarizedions/polarizedbot/commands/CommandManager.java
@@ -1,39 +1,21 @@
 package net.polarizedions.polarizedbot.commands;
 
 import net.polarizedions.polarizedbot.commands.builder.CommandTree;
-import net.polarizedions.polarizedbot.commands.impl.CommandAbout;
-import net.polarizedions.polarizedbot.commands.impl.CommandAnnoucer;
-import net.polarizedions.polarizedbot.commands.impl.CommandGuild;
-import net.polarizedions.polarizedbot.commands.impl.CommandHelp;
-import net.polarizedions.polarizedbot.commands.impl.CommandIgnore;
-import net.polarizedions.polarizedbot.commands.impl.CommandInvite;
-import net.polarizedions.polarizedbot.commands.impl.CommandPing;
-import net.polarizedions.polarizedbot.commands.impl.CommandSay;
-import net.polarizedions.polarizedbot.commands.impl.CommandShutdown;
-import net.polarizedions.polarizedbot.commands.impl.CommandUpdate;
-import net.polarizedions.polarizedbot.commands.impl.CommandWolframAlpha;
+import net.polarizedions.polarizedbot.commands.impl.*;
 import net.polarizedions.polarizedbot.config.GuildConfig;
 import net.polarizedions.polarizedbot.exceptions.CommandException;
 import net.polarizedions.polarizedbot.util.GuildManager;
 import net.polarizedions.polarizedbot.util.Localizer;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import sx.blah.discord.api.IDiscordClient;
-import sx.blah.discord.api.events.IListener;
-import sx.blah.discord.handle.impl.events.guild.channel.message.MessageReceivedEvent;
 import sx.blah.discord.handle.obj.IGuild;
 import sx.blah.discord.handle.obj.IMessage;
 import sx.blah.discord.handle.obj.IUser;
 import sx.blah.discord.util.DiscordException;
 import sx.blah.discord.util.MissingPermissionsException;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
+import java.util.concurrent.CompletableFuture;
 
 public class CommandManager {
     private Logger logger = LogManager.getLogger("CommandManager");
@@ -64,14 +46,15 @@ public class CommandManager {
         IUser user = message.getAuthor();
         IGuild guild = message.getGuild();
 
+
         if (guild == null) {
+            Localizer.setCurrentLang(Localizer.AVAILABLE_LANGS[0]);
             message.getChannel().sendMessage(Localizer.localize("error.pm_not_supported"));
             return;
         }
 
-        logger.debug("[UserID: {}, GuildID: {}, MessageID: {}, User: {}]: {}", message.getAuthor().getStringID(), message.getGuild().getStringID(), message.getStringID(), message.getAuthor().getName() + "#" + message.getAuthor().getDiscriminator(), message.getContent());
-
         GuildConfig guildConfig = GuildManager.getConfig(guild);
+        Localizer.setCurrentLang(guildConfig.lang);
 
         if (guildConfig.ignoredUsers.contains(user.getLongID())) {
             logger.debug("From ignored user");
@@ -79,7 +62,6 @@ public class CommandManager {
         }
 
         if (!message.getContent().startsWith(guildConfig.commandPrefix)) {
-            logger.debug("not command");
             return;
         }
 
@@ -91,8 +73,7 @@ public class CommandManager {
 
         CommandTree commandTree = this.commands.get(command);
         if (commandTree == null) {
-            logger.debug("no command found");
-            message.getChannel().sendMessage("no command found");
+            message.getChannel().sendMessage(Localizer.localize("error.no_such_command"));
             return;
         }
 
@@ -101,39 +82,53 @@ public class CommandManager {
             return;
         }
 
-        Localizer.setCurrentLang(guildConfig.lang);
-        logger.debug("Running command {}, alias {}, fragments: {}", commandTree.getName(), command, commandFragments);
-        message.getChannel().setTypingStatus(true);
-        try {
-            commandTree.execute(commandFragments, message);
-        }
-        catch (CommandException ex) {
-            logger.warn("Failed to handle command: threw {}", ex.getClass().getSimpleName());
-            message.getChannel().sendMessage(ex.getError());
-        }
-        catch (MissingPermissionsException ex) {
-            logger.error("No permission", ex.getMissingPermissions());
+        CompletableFuture.runAsync(() -> {
+            message.getChannel().setTypingStatus(true);
+            logger.debug("Running command {}, alias {}, fragments: {}", commandTree.getName(), command, commandFragments);
+
             try {
-                message.getChannel().sendMessage(Localizer.localize("error.no_permission", ex.getMissingPermissions()));
+                commandTree.execute(commandFragments, message);
+            }
+            catch (Exception ex) {
+                this.handleCommandException(message, ex);
+            }
+
+            message.getChannel().setTypingStatus(false);
+        });
+    }
+
+    private void handleCommandException(IMessage message, Exception ex) {
+        if (ex instanceof CommandException) {
+            logger.warn("Failed to handle command: threw {}", ex.getClass().getSimpleName());
+            message.getChannel().sendMessage(((CommandException) ex).getError());
+        }
+        else if (ex instanceof MissingPermissionsException) {
+            String neededPerms = ((MissingPermissionsException) ex).getMissingPermissions().toString();
+            logger.error("No permission", neededPerms);
+
+            String localized = Localizer.localize("error.no_permission", neededPerms);
+            try {
+                message.getChannel().sendMessage(localized);
             } catch(Exception e) {
                 try {
-                message.getAuthor().getOrCreatePMChannel().sendMessage(Localizer.localize("error.no_permission", ex.getErrorMessage()));}catch(Exception exc) {}
+                    message.getAuthor().getOrCreatePMChannel().sendMessage(localized);
+                }
+                catch(Exception exc) { /* NOOP */ }
             }
         }
-        catch (DiscordException ex) {
+        else if (ex instanceof DiscordException) {
             logger.error("Discord exception!", ex);
             try {
-                message.getChannel().sendMessage(Localizer.localize("error.discord_error", ex.getErrorMessage()));
-            } catch(Exception e) {}
+                message.getChannel().sendMessage(Localizer.localize("error.discord_error", ((DiscordException) ex).getErrorMessage()));
+            } catch(Exception e) { /* NOOP */ }
         }
-        catch (Exception ex) {
+        else {
             logger.error("I f'ed up", ex);
             try {
                 message.getChannel().sendMessage(Localizer.localize("error.misc_error", ex.getClass().getCanonicalName() + ": " + ex.getMessage()));
-            } catch(Exception e) {}
+            } catch(Exception e) { /* NOOP */ }
         }
 
-        message.getChannel().setTypingStatus(false);
     }
 
     public Set<CommandTree> getCommands() {

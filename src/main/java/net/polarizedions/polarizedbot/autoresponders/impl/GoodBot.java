031357d46cb8e19f1652de0d475d937901164084
PolarizedIons <polarizedions@gmail.com>
2018-10-15 14:39:04 +0200

    change 'good bot' matching slightly

--- src/main/java/net/polarizedions/polarizedbot/autoresponders/impl/GoodBot.java
+++ src/main/java/net/polarizedions/polarizedbot/autoresponders/impl/GoodBot.java
@@ -3,6 +3,7 @@ package net.polarizedions.polarizedbot.autoresponders.impl;
 import net.polarizedions.polarizedbot.Bot;
 import net.polarizedions.polarizedbot.autoresponders.IResponder;
 import net.polarizedions.polarizedbot.util.MessageUtil;
+import org.jetbrains.annotations.NotNull;
 import sx.blah.discord.handle.obj.IMessage;
 import sx.blah.discord.handle.obj.IUser;
 import sx.blah.discord.util.MessageHistory;
@@ -13,16 +14,27 @@ public class GoodBot implements IResponder {
         return "goodbot";
     }
 
-    @Override
-    public void run(IMessage message) {
-        String content = message.getContent();
+    private boolean inReplyToUs(@NotNull IMessage message) {
         IUser ourUser = Bot.instance.getClient().getOurUser();
-        if (! content.toLowerCase().equals("good bot") && !content.toLowerCase().equals("good " + ourUser.mention())) {
-            return;
+        MessageHistory messageHistory = message.getChannel().getMessageHistory(2);
+
+        if (messageHistory.size() < 2) {
+            return false;
         }
 
-        MessageHistory messageHistory = message.getChannel().getMessageHistory(2);
-        if (messageHistory.get(1).getAuthor().equals(ourUser)) {
+        return messageHistory.get(1).getAuthor().equals(ourUser);
+    }
+
+    @Override
+    public void run(IMessage message) {
+        String content = message.getContent().toLowerCase();
+        IUser ourUser = Bot.instance.getClient().getOurUser();
+        boolean isInReplyToUs = this.inReplyToUs(message);
+
+        if (( isInReplyToUs && content.startsWith("good bot") ) ||
+                ( content.startsWith("good " + ourUser.mention()) ) ||
+                ( content.startsWith(ourUser.mention() + " good bot") )
+        ) {
             MessageUtil.replyUnlocalized(message.getChannel(), "❤️");
         }
     }
